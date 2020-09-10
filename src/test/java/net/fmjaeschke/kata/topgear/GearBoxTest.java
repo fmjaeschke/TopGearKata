dca95076c9114dadd4b8fc64a16217c995816683
Frank-Michael Jaeschke <15345365+fmjaeschke@users.noreply.github.com>
2020-09-10 12:48:03 +0200

    Initial src and test commit Updated gitignore file

---
+++ src/test/java/net/fmjaeschke/kata/topgear/GearBoxTest.java
@@ -0,0 +1,12 @@
+package net.fmjaeschke.kata.topgear;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class GearBoxTest {
+
+    @Test
+    public void testNothing() {
+        Assert.assertTrue(true);
+    }
+}
